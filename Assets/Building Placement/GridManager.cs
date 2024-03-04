fd082f25de80fd11327b277517c9ac59b24d8570
cscmieu <115281028+cscmieu@users.noreply.github.com>
2024-03-04 17:48:14 +0100

    Created Singletons scripts
    Undestructible singleton goes into dontdestroyonload
    Simple Singleton doesn't

---
+++ Assets/Building Placement/GridManager.cs
@@ -0,0 +1,9 @@
+using Singletons;
+
+namespace Building_Placement
+{
+    public class GridManager : SimpleSingleton<GridManager>
+    {
+	    
+    }
+}
---
+++ Assets/Singletons/SimpleSingleton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace Singletons
+{
+    public class SimpleSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance is not null) return _instance;
+                _instance = (T)FindObjectOfType(typeof(T));
+                if (_instance is not null) return _instance;
+                SetupIstance();
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            RemoveDuplicates();
+        }
+
+        private static void SetupIstance()
+        {
+            var blankGameObject = new GameObject 
+                                  {
+                                      name = typeof(T).Name
+                                  };
+            _instance = blankGameObject.AddComponent<T>();
+        }
+        private void RemoveDuplicates()
+        {
+            if (_instance is null)
+            {
+                _instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
